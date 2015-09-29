e70b1978e5977ba8d078fa1a90acef8fd125a5d5
Artur Dudzik <dudziaso@gmail.com>
2015-09-29 20:39:22 +0200

    [MERGE DONE ALL CONFLICTS RESOLVED} DELETED PRODUCTS Update-Database !!!!

--- TestProject/Models/IdentityModels.cs
+++ TestProject/Models/IdentityModels.cs
@@ -36,7 +36,6 @@ namespace TestProject.Models
             return new ApplicationDbContext();
         }
 
-        public System.Data.Entity.DbSet<TestProject.Models.Product> Products { get; set; }
 
         public System.Data.Entity.DbSet<TestProject.Models.SecureObject> SecureObjects { get; set; }
 
